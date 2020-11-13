fa639282f9a1194ab546ffc19eefba30d32754d7
Emile Swain <Emile.Swain@akqa.com>
2020-11-13 09:46:13 +0000

    fixed? build config?

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import Control from './Control'
 import Preview from './Preview'
 import ARAssetEditorComponent from './ARAssetEditorComponent'
 
-if (typeof window !== 'undefined') {
-    window.ARAssetControl = Control
-    window.ARAssetPreview = Preview
-    window.ARAssetEditorComponent = ARAssetEditorComponent
-}
+// if (typeof window !== 'undefined') {
+//     window.ARAssetControl = Control
+//     window.ARAssetPreview = Preview
+//     window.ARAssetEditorComponent = ARAssetEditorComponent
+// }
 
 export { Control as ARAssetControl, Preview as ARAssetPreview , ARAssetEditorComponent as ARAssetEditorComponent}
